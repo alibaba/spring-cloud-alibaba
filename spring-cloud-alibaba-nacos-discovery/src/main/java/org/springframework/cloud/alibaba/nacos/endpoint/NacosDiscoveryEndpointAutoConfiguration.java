e4d9f98313bd3e825344df980a7299d3f115d6f0
flystar32 <flystar32@163.com>
2018-09-20 11:31:50 +0800

    update nacos version to 0.2.1-RC and format code

--- spring-cloud-alibaba-nacos-discovery/src/main/java/org/springframework/cloud/alibaba/nacos/endpoint/NacosDiscoveryEndpointAutoConfiguration.java
+++ spring-cloud-alibaba-nacos-discovery/src/main/java/org/springframework/cloud/alibaba/nacos/endpoint/NacosDiscoveryEndpointAutoConfiguration.java
@@ -20,19 +20,21 @@ import org.springframework.boot.actuate.autoconfigure.endpoint.condition.Conditi
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author xiaojing
  */
-@ConditionalOnWebApplication
+@Configuration
+@ConditionalOnProperty(value = "spring.cloud.service-registry.auto-registration.enabled", matchIfMissing = true)
 @ConditionalOnClass(Endpoint.class)
 public class NacosDiscoveryEndpointAutoConfiguration {
 
+	@Bean
 	@ConditionalOnMissingBean
 	@ConditionalOnEnabledEndpoint
-	@Bean
 	public NacosDiscoveryEndpoint nacosDiscoveryEndpoint() {
 		return new NacosDiscoveryEndpoint();
 	}
