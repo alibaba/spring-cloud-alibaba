7fdd8fa3e203f5d640dd105986dac392e7f12833
flystar32 <flystar32@163.com>
2018-10-11 21:48:35 +0800

    update sentinel version to 0.2.0

--- spring-cloud-alibaba-examples/sentinel-example/sentinel-core-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/JsonFlowRuleListParser.java
+++ spring-cloud-alibaba-examples/sentinel-example/sentinel-core-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/JsonFlowRuleListParser.java
@@ -2,7 +2,7 @@ package org.springframework.cloud.alibaba.cloud.examples;
 
 import java.util.List;
 
-import com.alibaba.csp.sentinel.datasource.ConfigParser;
+import com.alibaba.csp.sentinel.datasource.Converter;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
@@ -10,9 +10,10 @@ import com.alibaba.fastjson.TypeReference;
 /**
  * @author fangjian
  */
-public class JsonFlowRuleListParser implements ConfigParser<String, List<FlowRule>> {
+public class JsonFlowRuleListParser implements Converter<String, List<FlowRule>> {
 	@Override
-	public List<FlowRule> parse(String source) {
-		return JSON.parseObject(source, new TypeReference<List<FlowRule>>() {});
+	public List<FlowRule> convert(String source) {
+		return JSON.parseObject(source, new TypeReference<List<FlowRule>>() {
+		});
 	}
 }
