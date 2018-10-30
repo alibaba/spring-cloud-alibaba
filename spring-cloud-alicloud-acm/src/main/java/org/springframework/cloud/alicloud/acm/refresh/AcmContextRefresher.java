397d6e063a80bcf1c5553843f51b6a7dd6d75fad
xiaolongzuo <150349407@qq.com>
2018-10-30 16:16:29 +0800

    Extract acm configuration to bootstrap phase.
    # Conflicts:
    #	spring-cloud-alicloud-acm/src/main/java/org/springframework/cloud/alicloud/acm/refresh/AcmContextRefresher.java

--- spring-cloud-alicloud-acm/src/main/java/org/springframework/cloud/alicloud/acm/refresh/AcmContextRefresher.java
+++ spring-cloud-alicloud-acm/src/main/java/org/springframework/cloud/alicloud/acm/refresh/AcmContextRefresher.java
@@ -91,28 +91,31 @@ public class AcmContextRefresher implements ApplicationListener<ApplicationReady
 	}
 
 	private void registerDiamondListener(final String dataId) {
-
-		ConfigChangeListener listener = listenerMap.computeIfAbsent(dataId,
-				i -> new ConfigChangeListener() {
-					@Override
-					public void receiveConfigInfo(String configInfo) {
-						String md5 = "";
-						if (!StringUtils.isEmpty(configInfo)) {
-							try {
-								MessageDigest md = MessageDigest.getInstance("MD5");
-								md5 = new BigInteger(1,
-										md.digest(configInfo.getBytes("UTF-8")))
-												.toString(16);
-							}
-							catch (NoSuchAlgorithmException
-									| UnsupportedEncodingException e) {
-								logger.warn("unable to get md5 for dataId: " + dataId, e);
-							}
+		ConfigChangeListener listener = listenerMap.get(dataId);
+		if (listener == null) {
+			listener = new ConfigChangeListener() {
+				@Override
+				public void receiveConfigInfo(String configInfo) {
+					String md5 = "";
+					if (!StringUtils.isEmpty(configInfo)) {
+						try {
+							MessageDigest md = MessageDigest.getInstance("MD5");
+							md5 = new BigInteger(1,
+									md.digest(configInfo.getBytes("UTF-8"))).toString(16);
 						}
-						refreshHistory.add(dataId, md5);
-						contextRefresher.refresh();
+						catch (NoSuchAlgorithmException
+								| UnsupportedEncodingException e) {
+							logger.warn("unable to get md5 for dataId: " + dataId, e);
+						}
 					}
-				});
+					refreshHistory.add(dataId, md5);
+					contextRefresher.refresh();
+				}
+
+			};
+			listenerMap.put(dataId, listener);
+		}
+
 		ConfigService.addListener(dataId,
 				acmIntegrationProperties.getAcmProperties().getGroup(), listener);
 	}
