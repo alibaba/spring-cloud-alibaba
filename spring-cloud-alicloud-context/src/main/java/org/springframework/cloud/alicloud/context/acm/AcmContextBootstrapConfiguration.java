c309993fe092655454e64a4d938a8feb88ff4964
xiaolongzuo <150349407@qq.com>
2018-10-30 11:18:22 +0800

    optimize error message.

--- spring-cloud-alicloud-context/src/main/java/org/springframework/cloud/alicloud/context/acm/AcmContextBootstrapConfiguration.java
+++ spring-cloud-alicloud-context/src/main/java/org/springframework/cloud/alicloud/context/acm/AcmContextBootstrapConfiguration.java
@@ -66,7 +66,7 @@ public class AcmContextBootstrapConfiguration {
 		String applicationName = environment.getProperty("spring.application.name");
 		String applicationGroup = environment.getProperty("spring.application.group");
 		Assert.isTrue(!StringUtils.isEmpty(applicationName),
-				"'spring.application.name' must be configured..");
+				"'spring.application.name' must be configured in bootstrap.properties or bootstrap.yml/yaml...");
 		acmIntegrationProperties.setApplicationName(applicationName);
 		acmIntegrationProperties.setApplicationGroup(applicationGroup);
 		acmIntegrationProperties.setActiveProfiles(environment.getActiveProfiles());
