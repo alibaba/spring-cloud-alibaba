18e721ceedfca9a0c94b746fc0f8bb0e0af774eb
fangjian0423 <fangjian0423@gmail.com>
2018-11-27 13:55:38 +0800

    fixes #120 and do some refactor

--- spring-cloud-stream-binder-rocketmq/src/main/java/org/springframework/cloud/stream/binder/rocketmq/metrics/ConsumerGroupInstrumentation.java
+++ spring-cloud-stream-binder-rocketmq/src/main/java/org/springframework/cloud/stream/binder/rocketmq/metrics/ConsumerGroupInstrumentation.java
@@ -1,7 +1,5 @@
 package org.springframework.cloud.stream.binder.rocketmq.metrics;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 import com.codahale.metrics.MetricRegistry;
 
 /**
@@ -11,24 +9,9 @@ import com.codahale.metrics.MetricRegistry;
 public class ConsumerGroupInstrumentation extends Instrumentation {
 	private MetricRegistry metricRegistry;
 
-	private AtomicBoolean delayedStart = new AtomicBoolean(false);
-
 	public ConsumerGroupInstrumentation(MetricRegistry metricRegistry, String name) {
 		super(name);
 		this.metricRegistry = metricRegistry;
 	}
 
-	public void markDelayedStart() {
-		delayedStart.set(true);
-	}
-
-	@Override
-	public boolean isUp() {
-		return started.get() || delayedStart.get();
-	}
-
-	@Override
-	public boolean isOutOfService() {
-		return !started.get() && startException == null && !delayedStart.get();
-	}
 }
