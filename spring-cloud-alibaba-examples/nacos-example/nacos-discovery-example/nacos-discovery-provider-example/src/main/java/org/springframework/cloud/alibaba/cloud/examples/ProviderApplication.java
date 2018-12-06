e9253f22c571bd5597dca4b1b9501d6580149b79
fangjian0423 <fangjian0423@gmail.com>
2018-12-06 14:16:47 +0800

    using sentinel feign in nacos discovery example

--- spring-cloud-alibaba-examples/nacos-example/nacos-discovery-example/nacos-discovery-provider-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/ProviderApplication.java
+++ spring-cloud-alibaba-examples/nacos-example/nacos-discovery-example/nacos-discovery-provider-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/ProviderApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,6 +25,11 @@ public class ProviderApplication {
 		@RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
 		public String echo(@PathVariable String string) {
 			return "hello Nacos Discovery " + string;
+		}
+
+		@RequestMapping(value = "/divide", method = RequestMethod.GET)
+		public String divide(@RequestParam Integer a, @RequestParam Integer b) {
+			return String.valueOf(a / b);
 		}
 	}
 }
