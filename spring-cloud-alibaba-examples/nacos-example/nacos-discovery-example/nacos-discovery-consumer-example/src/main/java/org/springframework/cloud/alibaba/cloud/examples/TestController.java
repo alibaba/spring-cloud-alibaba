be2f52c88003680d0bc170fb82e005d849ee1be3
fangjian0423 <fangjian0423@gmail.com>
2018-12-06 17:11:50 +0800

    using sentinel feign in nacos discovery example

--- spring-cloud-alibaba-examples/nacos-example/nacos-discovery-example/nacos-discovery-consumer-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/TestController.java
+++ spring-cloud-alibaba-examples/nacos-example/nacos-discovery-example/nacos-discovery-consumer-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,31 +16,42 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class TestController {
 
-    @Autowired
-    private RestTemplate restTemplate;
-    @Autowired
-    private EchoService echoService;
+	@Autowired
+	private RestTemplate restTemplate;
+	@Autowired
+	private EchoService echoService;
 
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+	@RequestMapping(value = "/echo-rest/{str}", method = RequestMethod.GET)
+	public String rest(@PathVariable String str) {
+		return restTemplate.getForObject("http://service-provider/echo/" + str,
+				String.class);
+	}
 
-    @RequestMapping(value = "/echo-rest/{str}", method = RequestMethod.GET)
-    public String rest(@PathVariable String str) {
-        return restTemplate.getForObject("http://service-provider/echo/" + str, String.class);
-    }
-    @RequestMapping(value = "/echo-feign/{str}", method = RequestMethod.GET)
-    public String feign(@PathVariable String str) {
-        return echoService.echo(str);
-    }
+	@RequestMapping(value = "/notFound-feign", method = RequestMethod.GET)
+	public String notFound() {
+		return echoService.notFound();
+	}
 
+	@RequestMapping(value = "/divide-feign", method = RequestMethod.GET)
+	public String divide(@RequestParam Integer a, @RequestParam Integer b) {
+		return echoService.divide(a, b);
+	}
 
-    @RequestMapping(value="/services/{service}",method = RequestMethod.GET)
-    public Object client(@PathVariable String service){
-        return discoveryClient.getInstances(service);
-    }
-    @RequestMapping(value="/services",method = RequestMethod.GET)
-    public Object services(){
-        return discoveryClient.getServices();
-    }
+	@RequestMapping(value = "/echo-feign/{str}", method = RequestMethod.GET)
+	public String feign(@PathVariable String str) {
+		return echoService.echo(str);
+	}
+
+	@RequestMapping(value = "/services/{service}", method = RequestMethod.GET)
+	public Object client(@PathVariable String service) {
+		return discoveryClient.getInstances(service);
+	}
+
+	@RequestMapping(value = "/services", method = RequestMethod.GET)
+	public Object services() {
+		return discoveryClient.getServices();
+	}
 }
