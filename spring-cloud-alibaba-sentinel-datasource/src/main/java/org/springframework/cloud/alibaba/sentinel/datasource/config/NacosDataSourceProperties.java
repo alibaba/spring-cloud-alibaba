bf40e5261dffb8c63e084d523fe8e6c26481626f
fangjian0423 <fangjian0423@gmail.com>
2018-12-19 17:39:32 +0800

    refactor sentinel datasource and support commercialized

--- spring-cloud-alibaba-sentinel-datasource/src/main/java/org/springframework/cloud/alibaba/sentinel/datasource/config/NacosDataSourceProperties.java
+++ spring-cloud-alibaba-sentinel-datasource/src/main/java/org/springframework/cloud/alibaba/sentinel/datasource/config/NacosDataSourceProperties.java
@@ -1,6 +1,9 @@
 package org.springframework.cloud.alibaba.sentinel.datasource.config;
 
+import org.springframework.cloud.alibaba.sentinel.datasource.SentinelDataSourceConstants;
 import org.springframework.cloud.alibaba.sentinel.datasource.factorybean.NacosDataSourceFactoryBean;
+import org.springframework.cloud.alibaba.sentinel.datasource.factorybean.NacosDataSourceWithAuthorizationFactoryBean;
+import org.springframework.util.StringUtils;
 
 /**
  * Nacos Properties class Using by {@link DataSourcePropertiesConfiguration} and
@@ -14,8 +17,33 @@ public class NacosDataSourceProperties extends AbstractDataSourceProperties {
 	private String groupId;
 	private String dataId;
 
+	// commercialized usage
+
+	private String endpoint;
+	private String namespace;
+	private String accessKey;
+	private String secretKey;
+
 	public NacosDataSourceProperties() {
 		super(NacosDataSourceFactoryBean.class.getName());
+	}
+
+	@Override
+	public void preCheck() {
+		if (!StringUtils.isEmpty(System.getProperties()
+				.getProperty(SentinelDataSourceConstants.NACOS_DATASOURCE_ENDPOINT))) {
+			this.setServerAddr(null);
+			this.setFactoryBeanName(
+					NacosDataSourceWithAuthorizationFactoryBean.class.getName());
+			this.setEndpoint(System.getProperties()
+					.getProperty(SentinelDataSourceConstants.NACOS_DATASOURCE_ENDPOINT));
+			this.setNamespace(System.getProperties()
+					.getProperty(SentinelDataSourceConstants.NACOS_DATASOURCE_NAMESPACE));
+			this.setAccessKey(System.getProperties()
+					.getProperty(SentinelDataSourceConstants.NACOS_DATASOURCE_AK));
+			this.setSecretKey(System.getProperties()
+					.getProperty(SentinelDataSourceConstants.NACOS_DATASOURCE_SK));
+		}
 	}
 
 	public String getServerAddr() {
@@ -40,5 +68,37 @@ public class NacosDataSourceProperties extends AbstractDataSourceProperties {
 
 	public void setDataId(String dataId) {
 		this.dataId = dataId;
+	}
+
+	public String getEndpoint() {
+		return endpoint;
+	}
+
+	public void setEndpoint(String endpoint) {
+		this.endpoint = endpoint;
+	}
+
+	public String getNamespace() {
+		return namespace;
+	}
+
+	public void setNamespace(String namespace) {
+		this.namespace = namespace;
+	}
+
+	public String getAccessKey() {
+		return accessKey;
+	}
+
+	public void setAccessKey(String accessKey) {
+		this.accessKey = accessKey;
+	}
+
+	public String getSecretKey() {
+		return secretKey;
+	}
+
+	public void setSecretKey(String secretKey) {
+		this.secretKey = secretKey;
 	}
 }
