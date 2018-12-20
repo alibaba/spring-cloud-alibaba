5eb414cdca429c9ab20ed7dbbbffdafd03472b97
fangjian0423 <fangjian0423@gmail.com>
2018-12-20 21:46:56 +0800

    update docs

--- spring-cloud-alibaba-sentinel/src/main/java/org/springframework/cloud/alibaba/sentinel/SentinelProperties.java
+++ spring-cloud-alibaba-sentinel/src/main/java/org/springframework/cloud/alibaba/sentinel/SentinelProperties.java
@@ -244,7 +244,7 @@ public class SentinelProperties {
 	public static class Transport {
 
 		/**
-		 * sentinel api port,default value is 8721 {@link TransportConfig#SERVER_PORT}
+		 * sentinel api port,default value is 8719 {@link TransportConfig#SERVER_PORT}
 		 */
 		private String port = "8719";
 
