1fb33911f32aceb50a6ec8ee7adf34fad62a5b83
fangjian0423 <fangjian0423@gmail.com>
2019-01-11 15:43:18 +0800

    refactor static final field name

--- spring-cloud-alibaba-sentinel/src/main/java/org/springframework/cloud/alibaba/sentinel/custom/SentinelDataSourceHandler.java
+++ spring-cloud-alibaba-sentinel/src/main/java/org/springframework/cloud/alibaba/sentinel/custom/SentinelDataSourceHandler.java
@@ -54,9 +54,9 @@ public class SentinelDataSourceHandler {
 	private List<String> dataSourceBeanNameList = Collections
 			.synchronizedList(new ArrayList<>());
 
-	private final String DATATYPE_FIELD = "dataType";
-	private final String CUSTOM_DATATYPE = "custom";
-	private final String CONVERTERCLASS_FIELD = "converterClass";
+	private final String DATA_TYPE_FIELD = "dataType";
+	private final String CUSTOM_DATA_TYPE = "custom";
+	private final String CONVERTER_CLASS_FIELD = "converterClass";
 
 	@Autowired
 	private SentinelProperties sentinelProperties;
@@ -132,8 +132,8 @@ public class SentinelDataSourceHandler {
 								e);
 					}
 				}, HashMap::putAll);
-		propertyMap.put(CONVERTERCLASS_FIELD, dataSourceProperties.getConverterClass());
-		propertyMap.put(DATATYPE_FIELD, dataSourceProperties.getDataType());
+		propertyMap.put(CONVERTER_CLASS_FIELD, dataSourceProperties.getConverterClass());
+		propertyMap.put(DATA_TYPE_FIELD, dataSourceProperties.getDataType());
 
 		BeanDefinitionBuilder builder = BeanDefinitionBuilder
 				.genericBeanDefinition(dataSourceProperties.getFactoryBeanName());
@@ -141,75 +141,71 @@ public class SentinelDataSourceHandler {
 		propertyMap.forEach((propertyName, propertyValue) -> {
 			Field field = ReflectionUtils.findField(dataSourceProperties.getClass(),
 					propertyName);
-			if (field != null) {
-				if (DATATYPE_FIELD.equals(propertyName)) {
-					String dataType = StringUtils
-							.trimAllWhitespace(propertyValue.toString());
-					if (CUSTOM_DATATYPE.equals(dataType)) {
-						try {
-							if (StringUtils
-									.isEmpty(dataSourceProperties.getConverterClass())) {
-								throw new RuntimeException(
-										"[Sentinel Starter] DataSource " + dataSourceName
-												+ "dataType is custom, please set converter-class "
-												+ "property");
-							}
-							// construct custom Converter with 'converterClass'
-							// configuration and register
-							String customConvertBeanName = "sentinel-"
-									+ dataSourceProperties.getConverterClass();
-							if (!beanFactory.containsBean(customConvertBeanName)) {
-								beanFactory.registerBeanDefinition(customConvertBeanName,
-										BeanDefinitionBuilder
-												.genericBeanDefinition(
-														Class.forName(dataSourceProperties
-																.getConverterClass()))
-												.getBeanDefinition());
-							}
-							builder.addPropertyReference("converter",
-									customConvertBeanName);
-						}
-						catch (ClassNotFoundException e) {
-							logger.error("[Sentinel Starter] DataSource " + dataSourceName
-									+ " handle "
-									+ dataSourceProperties.getClass().getSimpleName()
-									+ " error, class name: "
-									+ dataSourceProperties.getConverterClass());
-							throw new RuntimeException(
-									"[Sentinel Starter] DataSource " + dataSourceName
-											+ " handle "
-											+ dataSourceProperties.getClass()
-													.getSimpleName()
-											+ " error, class name: "
-											+ dataSourceProperties.getConverterClass(),
-									e);
-						}
-					}
-					else {
-						if (!dataTypeList.contains(StringUtils
-								.trimAllWhitespace(propertyValue.toString()))) {
+			if (null == field) {
+				return;
+			}
+			if (DATA_TYPE_FIELD.equals(propertyName)) {
+				String dataType = StringUtils.trimAllWhitespace(propertyValue.toString());
+				if (CUSTOM_DATA_TYPE.equals(dataType)) {
+					try {
+						if (StringUtils
+								.isEmpty(dataSourceProperties.getConverterClass())) {
 							throw new RuntimeException("[Sentinel Starter] DataSource "
-									+ dataSourceName + " dataType: " + propertyValue
-									+ " is not support now. please using these types: "
-									+ dataTypeList.toString());
+									+ dataSourceName
+									+ "dataType is custom, please set converter-class "
+									+ "property");
 						}
-						// converter type now support xml or json.
-						// The bean name of these converters wrapped by
-						// 'sentinel-{converterType}-{ruleType}-converter'
-						builder.addPropertyReference("converter",
-								"sentinel-" + propertyValue.toString() + "-"
-										+ dataSourceProperties.getRuleType().getName()
-										+ "-converter");
+						// construct custom Converter with 'converterClass'
+						// configuration and register
+						String customConvertBeanName = "sentinel-"
+								+ dataSourceProperties.getConverterClass();
+						if (!beanFactory.containsBean(customConvertBeanName)) {
+							beanFactory.registerBeanDefinition(customConvertBeanName,
+									BeanDefinitionBuilder
+											.genericBeanDefinition(
+													Class.forName(dataSourceProperties
+															.getConverterClass()))
+											.getBeanDefinition());
+						}
+						builder.addPropertyReference("converter", customConvertBeanName);
 					}
-				}
-				else if (CONVERTERCLASS_FIELD.equals(propertyName)) {
-					return;
+					catch (ClassNotFoundException e) {
+						logger.error("[Sentinel Starter] DataSource " + dataSourceName
+								+ " handle "
+								+ dataSourceProperties.getClass().getSimpleName()
+								+ " error, class name: "
+								+ dataSourceProperties.getConverterClass());
+						throw new RuntimeException("[Sentinel Starter] DataSource "
+								+ dataSourceName + " handle "
+								+ dataSourceProperties.getClass().getSimpleName()
+								+ " error, class name: "
+								+ dataSourceProperties.getConverterClass(), e);
+					}
 				}
 				else {
-					// wired properties
-					Optional.ofNullable(propertyValue)
-							.ifPresent(v -> builder.addPropertyValue(propertyName, v));
+					if (!dataTypeList.contains(
+							StringUtils.trimAllWhitespace(propertyValue.toString()))) {
+						throw new RuntimeException("[Sentinel Starter] DataSource "
+								+ dataSourceName + " dataType: " + propertyValue
+								+ " is not support now. please using these types: "
+								+ dataTypeList.toString());
+					}
+					// converter type now support xml or json.
+					// The bean name of these converters wrapped by
+					// 'sentinel-{converterType}-{ruleType}-converter'
+					builder.addPropertyReference("converter",
+							"sentinel-" + propertyValue.toString() + "-"
+									+ dataSourceProperties.getRuleType().getName()
+									+ "-converter");
 				}
+			}
+			else if (CONVERTER_CLASS_FIELD.equals(propertyName)) {
+				return;
+			}
+			else {
+				// wired properties
+				Optional.ofNullable(propertyValue)
+						.ifPresent(v -> builder.addPropertyValue(propertyName, v));
 			}
 		});
 
