1c375f13285bf6b79b9a5f00475dbde539be82ce
fangjian0423 <fangjian0423@gmail.com>
2019-01-14 21:00:17 +0800

    refactor sentinel DataSourceHandler

--- spring-cloud-alibaba-sentinel/src/main/java/org/springframework/cloud/alibaba/sentinel/custom/SentinelAutoConfiguration.java
+++ spring-cloud-alibaba-sentinel/src/main/java/org/springframework/cloud/alibaba/sentinel/custom/SentinelAutoConfiguration.java
@@ -22,6 +22,7 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -165,8 +166,9 @@ public class SentinelAutoConfiguration {
 	}
 
 	@Bean
-	public SentinelDataSourceHandler sentinelDataSourceHandler() {
-		return new SentinelDataSourceHandler();
+	public SentinelDataSourceHandler sentinelDataSourceHandler(
+			DefaultListableBeanFactory beanFactory) {
+		return new SentinelDataSourceHandler(beanFactory);
 	}
 
 	protected static class SentinelConverterConfiguration {
