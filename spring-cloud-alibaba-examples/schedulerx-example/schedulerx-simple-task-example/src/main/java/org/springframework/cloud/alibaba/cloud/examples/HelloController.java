3c49dbf3bda9f0beaa6a8ed390a36d7ee640e923
xiaolongzuo <150349407@qq.com>
2019-02-25 14:35:39 +0800

    Polish test cases for schedulerx

--- spring-cloud-alibaba-examples/schedulerx-example/schedulerx-simple-task-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/HelloController.java
+++ spring-cloud-alibaba-examples/schedulerx-example/schedulerx-simple-task-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/HelloController.java
@@ -16,25 +16,20 @@
 
 package org.springframework.cloud.alibaba.cloud.examples;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.alibaba.edas.schedulerx.ProcessResult;
-import com.alibaba.edas.schedulerx.ScxSimpleJobContext;
-import com.alibaba.edas.schedulerx.ScxSimpleJobProcessor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xiaolongzuo
  */
-public class SimpleTask implements ScxSimpleJobProcessor {
+@RestController
+public class HelloController {
 
-	@Autowired
-	private TestService testService;
-
-	@Override
-	public ProcessResult process(ScxSimpleJobContext context) {
-		testService.test();
-		ProcessResult processResult = new ProcessResult(true);
-		return processResult;
+	@RequestMapping("/")
+	@ResponseBody
+	public String hello() {
+		return "OK";
 	}
 
 }
