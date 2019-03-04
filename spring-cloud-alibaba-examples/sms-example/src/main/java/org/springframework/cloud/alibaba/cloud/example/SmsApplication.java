d0cbd9f99479cf889d7412cbf74ad26f06590c2c
fangjian0423 <fangjian0423@gmail.com>
2019-03-04 15:24:29 +0800

    sms refactor 1.x

--- spring-cloud-alibaba-examples/sms-example/src/main/java/org/springframework/cloud/alibaba/cloud/example/SmsApplication.java
+++ spring-cloud-alibaba-examples/sms-example/src/main/java/org/springframework/cloud/alibaba/cloud/example/SmsApplication.java
@@ -1,11 +1,9 @@
 package org.springframework.cloud.alibaba.cloud.example;
 
-import org.springframework.alicloud.env.extension.ImportExtraConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ImportExtraConfig(name = "/Users/toava/sms.properties")
 public class SmsApplication {
 
 	public static void main(String[] args) {
