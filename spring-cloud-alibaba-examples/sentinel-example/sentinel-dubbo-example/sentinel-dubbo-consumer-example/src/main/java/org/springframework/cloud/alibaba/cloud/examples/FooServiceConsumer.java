ade58496fc92ccf6536864bbed320ffe5cbace27
fangjian0423 <fangjian0423@gmail.com>
2019-04-17 11:13:22 +0800

    sync code with dubbo version in finchley.

--- spring-cloud-alibaba-examples/sentinel-example/sentinel-dubbo-example/sentinel-dubbo-consumer-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/FooServiceConsumer.java
+++ spring-cloud-alibaba-examples/sentinel-example/sentinel-dubbo-example/sentinel-dubbo-consumer-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/FooServiceConsumer.java
@@ -1,14 +1,13 @@
 package org.springframework.cloud.alibaba.cloud.examples;
 
-import com.alibaba.dubbo.config.annotation.Reference;
+import org.apache.dubbo.config.annotation.Reference;
 
 /**
  * @author fangjian
  */
 public class FooServiceConsumer {
 
-	@Reference(version = "${foo.service.version}", application = "${dubbo.application.id}",
-            url = "dubbo://localhost:12345", timeout = 30000)
+	@Reference(version = "${foo.service.version}", application = "${dubbo.application.id}", url = "dubbo://localhost:12345", timeout = 30000)
 	private FooService fooService;
 
 	public String hello(String name) {
