e812061a75378dc9e83fc8b1b54e09479e9a1b08
fangjian0423 <fangjian0423@gmail.com>
2019-06-24 16:15:17 +0800

    update sentinel add hot param flow rule case

--- spring-cloud-alibaba-examples/sentinel-example/sentinel-core-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/TestController.java
+++ spring-cloud-alibaba-examples/sentinel-example/sentinel-core-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/TestController.java
@@ -23,10 +23,16 @@ public class TestController {
 		return "Hello";
 	}
 
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String test1() {
+	@RequestMapping(value = "/aa", method = RequestMethod.GET)
+    @SentinelResource("aa")
+	public String aa(int b, int a) {
 		return "Hello test";
 	}
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test1() {
+        return "Hello test";
+    }
 
 	@RequestMapping(value = "/template", method = RequestMethod.GET)
 	public String client() {
