525564cc22458c7a4f93ea1aadbe134cee84141a
caotc <250622148@qq.com>
2019-09-03 13:38:19 +0800

    format code
    Signed-off-by: caotc <250622148@qq.com>

--- spring-cloud-stream-binder-rocketmq/src/main/java/com/alibaba/cloud/stream/binder/rocketmq/support/RocketMQHeaderMapper.java
+++ spring-cloud-stream-binder-rocketmq/src/main/java/com/alibaba/cloud/stream/binder/rocketmq/support/RocketMQHeaderMapper.java
@@ -1,27 +1,28 @@
 package com.alibaba.cloud.stream.binder.rocketmq.support;
 
-import org.springframework.messaging.MessageHeaders;
-
 import java.util.Map;
 
+import org.springframework.messaging.MessageHeaders;
+
 /**
-* header value mapper for RocketMQ
-*
-* @author caotc
-* @date 2019-08-22
-* @since 2.1.1
-*/
+ * header value mapper for RocketMQ
+ *
+ * @author caotc
+ * @date 2019-08-22
+ * @since 2.1.1
+ */
 public interface RocketMQHeaderMapper {
-    /**
-     * Map from the given {@link MessageHeaders} to the specified target message.
-     * @param headers the abstracted MessageHeaders.
-     * @return  the native target message.
-     */
-    Map<String,String> fromHeaders(MessageHeaders headers);
-    /**
-     * Map from the given target message to abstracted {@link MessageHeaders}.
-     * @param source the native target message.
-     * @return  the target headers.
-     */
-    MessageHeaders toHeaders(Map<String,String> source);
+	/**
+	 * Map from the given {@link MessageHeaders} to the specified target message.
+	 * @param headers the abstracted MessageHeaders.
+	 * @return the native target message.
+	 */
+	Map<String, String> fromHeaders(MessageHeaders headers);
+
+	/**
+	 * Map from the given target message to abstracted {@link MessageHeaders}.
+	 * @param source the native target message.
+	 * @return the target headers.
+	 */
+	MessageHeaders toHeaders(Map<String, String> source);
 }
