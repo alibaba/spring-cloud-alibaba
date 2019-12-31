34d82670684025410fe3f183ba10e8d265a796a4
slievrly <slievrly@163.com>
2019-12-31 18:40:14 +0800

    adapter: use seata-spring-boot-starter's autoConfiguration instead of spring-cloud-alibaba-seata-starter's autoConfiguration
    Signed-off-by: slievrly <slievrly@163.com>

--- spring-cloud-alibaba-examples/seata-example/account-service/src/main/java/com/alibaba/cloud/examples/DatabaseConfiguration.java
+++ spring-cloud-alibaba-examples/seata-example/account-service/src/main/java/com/alibaba/cloud/examples/DatabaseConfiguration.java
@@ -16,15 +16,14 @@
 
 package com.alibaba.cloud.examples;
 
-import java.sql.SQLException;
+import javax.sql.DataSource;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import io.seata.rm.datasource.DataSourceProxy;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
@@ -33,61 +32,21 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @Configuration
 public class DatabaseConfiguration {
 
-	private final ApplicationContext applicationContext;
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSource storageDataSource() {
+        return new DruidDataSource();
+    }
 
-	public DatabaseConfiguration(ApplicationContext applicationContext) {
-		this.applicationContext = applicationContext;
-	}
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
-	@Bean(initMethod = "init", destroyMethod = "close")
-	public DruidDataSource storageDataSource() throws SQLException {
+        jdbcTemplate.update("delete from account_tbl where user_id = 'U100001'");
+        jdbcTemplate.update("insert into account_tbl(user_id, money) values ('U100001', 10000)");
 
-		Environment environment = applicationContext.getEnvironment();
-
-		String ip = environment.getProperty("mysql.server.ip");
-		String port = environment.getProperty("mysql.server.port");
-		String dbName = environment.getProperty("mysql.db.name");
-
-		String userName = environment.getProperty("mysql.user.name");
-		String password = environment.getProperty("mysql.user.password");
-
-		DruidDataSource druidDataSource = new DruidDataSource();
-		druidDataSource.setUrl(
-				"jdbc:mysql://" + ip + ":" + port + "/" + dbName + "?serverTimezone=UTC");
-		druidDataSource.setUsername(userName);
-		druidDataSource.setPassword(password);
-		druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		druidDataSource.setInitialSize(0);
-		druidDataSource.setMaxActive(180);
-		druidDataSource.setMaxWait(60000);
-		druidDataSource.setMinIdle(0);
-		druidDataSource.setValidationQuery("Select  'x' from DUAL");
-		druidDataSource.setTestOnBorrow(false);
-		druidDataSource.setTestOnReturn(false);
-		druidDataSource.setTestWhileIdle(true);
-		druidDataSource.setTimeBetweenEvictionRunsMillis(60000);
-		druidDataSource.setMinEvictableIdleTimeMillis(25200000);
-		druidDataSource.setRemoveAbandoned(true);
-		druidDataSource.setRemoveAbandonedTimeout(1800);
-		druidDataSource.setLogAbandoned(true);
-		druidDataSource.setFilters("mergeStat");
-		return druidDataSource;
-	}
-
-	@Bean
-	public DataSourceProxy dataSourceProxy(DruidDataSource druidDataSource) {
-		return new DataSourceProxy(druidDataSource);
-	}
-
-	@Bean
-	public JdbcTemplate jdbcTemplate(DataSourceProxy dataSourceProxy) {
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSourceProxy);
-
-		jdbcTemplate.update("delete from account_tbl where user_id = 'U100001'");
-		jdbcTemplate.update(
-				"insert into account_tbl(user_id, money) values ('U100001', 10000)");
-
-		return jdbcTemplate;
-	}
+        return jdbcTemplate;
+    }
 
 }
