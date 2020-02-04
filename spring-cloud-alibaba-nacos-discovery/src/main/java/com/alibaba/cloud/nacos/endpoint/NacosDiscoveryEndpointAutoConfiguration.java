d1e4711e98c48a9f4e76a5a83acfbb51d511efbc
mercyblitz <mercyblitz@gmail.com>
2020-02-04 16:21:55 +0800

    Polish alibaba/spring-cloud-alibaba/#902 : [Nacos Discovery] create NacosDiscoveryHealthIndicator

--- spring-cloud-alibaba-nacos-discovery/src/main/java/com/alibaba/cloud/nacos/endpoint/NacosDiscoveryEndpointAutoConfiguration.java
+++ spring-cloud-alibaba-nacos-discovery/src/main/java/com/alibaba/cloud/nacos/endpoint/NacosDiscoveryEndpointAutoConfiguration.java
@@ -18,9 +18,12 @@ package com.alibaba.cloud.nacos.endpoint;
 
 import com.alibaba.cloud.nacos.ConditionalOnNacosDiscoveryEnabled;
 import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.discovery.actuate.health.NacosDiscoveryHealthIndicator;
 
 import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnEnabledEndpoint;
+import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -42,4 +45,10 @@ public class NacosDiscoveryEndpointAutoConfiguration {
 		return new NacosDiscoveryEndpoint(nacosDiscoveryProperties);
 	}
 
+	@Bean
+	@ConditionalOnEnabledHealthIndicator("nacos-discovery")
+	public HealthIndicator nacosDiscoveryHealthIndicator(
+			NacosDiscoveryProperties nacosDiscoveryProperties) {
+		return new NacosDiscoveryHealthIndicator(nacosDiscoveryProperties);
+	}
 }
