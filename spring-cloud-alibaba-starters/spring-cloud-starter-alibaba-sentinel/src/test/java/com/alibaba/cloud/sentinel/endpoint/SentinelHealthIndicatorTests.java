eb09c52db57318b764dd3acbc3c09d7f038c9d56
祁晓波 <qixiaobo.nj@gmail.com>
2020-07-16 12:56:58 +0800

    Update SentinelHealthIndicatorTests.java
    update down status to unknown

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-sentinel/src/test/java/com/alibaba/cloud/sentinel/endpoint/SentinelHealthIndicatorTests.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-sentinel/src/test/java/com/alibaba/cloud/sentinel/endpoint/SentinelHealthIndicatorTests.java
@@ -109,9 +109,9 @@ public class SentinelHealthIndicatorTests {
 
 		Health health = sentinelHealthIndicator.health();
 
-		assertThat(health.getStatus()).isEqualTo(Status.DOWN);
+		assertThat(health.getStatus()).isEqualTo(Status.UNKNOWN);
 		assertThat(health.getDetails().get("dashboard")).isEqualTo(
-				new Status(Status.DOWN.getCode(), "localhost:8080 can't be connected"));
+				new Status(Status.UNKNOWN.getCode(), "localhost:8080 can't be connected"));
 	}
 
 	@Test
@@ -169,7 +169,7 @@ public class SentinelHealthIndicatorTests {
 		assertThat(dataSourceDetailMap.get("ds1-sentinel-file-datasource"))
 				.isEqualTo(Status.UP);
 		assertThat(dataSourceDetailMap.get("ds2-sentinel-file-datasource"))
-				.isEqualTo(new Status(Status.DOWN.getCode(), "fileDataSource2 error"));
+				.isEqualTo(new Status(Status.UNKNOWN.getCode(), "fileDataSource2 error"));
 	}
 
 }
