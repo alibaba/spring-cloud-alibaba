588720dce4b2d460c4b332ab837ca7e2ac460b2d
yuhuangbin <danielyu96@163.com>
2020-08-27 20:34:45 +0800

    fix code style

--- spring-cloud-alibaba-starters/spring-cloud-starter-dubbo/src/main/java/com/alibaba/cloud/dubbo/util/DubboCloudConstants.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-dubbo/src/main/java/com/alibaba/cloud/dubbo/util/DubboCloudConstants.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2013-2018 the original author or authors.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,18 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.alibaba.cloud.dubbo.util;
 
 /**
- * The constants for Dubbo Spring Cloud
+ * The constants for Dubbo Spring Cloud.
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  */
-public interface DubboCloudConstants {
+public final class DubboCloudConstants {
+
+	private DubboCloudConstants() {
+		throw new AssertionError("Must not instantiate constant utility class");
+	}
 
 	/**
-	 * The property prefix of Configuration
+	 * The property prefix of Configuration.
 	 */
-	String CONFIG_PROPERTY_PREFIX = "dubbo.cloud";
+	public static final String CONFIG_PROPERTY_PREFIX = "dubbo.cloud";
 
 }
