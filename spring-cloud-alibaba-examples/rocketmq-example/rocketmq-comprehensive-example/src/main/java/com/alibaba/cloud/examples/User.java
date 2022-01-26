2c796db6258f4cb609c29096ff811c5e434ca66a
Freeman Lau <llw599502537@gmail.com>
2022-01-26 14:48:02 +0800

    cherry-pick freeman Add rocketmq example

---
+++ spring-cloud-alibaba-examples/rocketmq-example/rocketmq-comprehensive-example/src/main/java/com/alibaba/cloud/examples/User.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2013-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.cloud.examples;
+
+import java.util.Map;
+
+/**
+ * @author freeman
+ */
+public class User {
+	private String id;
+	private String name;
+	private Map<String, Object> meta;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Map<String, Object> getMeta() {
+		return meta;
+	}
+
+	public void setMeta(Map<String, Object> meta) {
+		this.meta = meta;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" + "id='" + id + '\'' + ", name='" + name + '\'' + ", meta=" + meta
+				+ '}';
+	}
+}
