246419b687db78e759eb017a4f5f8a350cb72312
ruansheng <cearnach@foxmail.com>
2022-03-09 21:04:49 +0800

    Fix Dubbo MetaDataService NPE

--- spring-cloud-alibaba-starters/spring-cloud-starter-dubbo/src/main/java/com/alibaba/cloud/dubbo/service/IntrospectiveDubboMetadataService.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-dubbo/src/main/java/com/alibaba/cloud/dubbo/service/IntrospectiveDubboMetadataService.java
@@ -29,7 +29,6 @@ import org.apache.dubbo.common.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static java.util.Collections.unmodifiableMap;
@@ -44,16 +43,18 @@ public class IntrospectiveDubboMetadataService implements DubboMetadataService {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	@Autowired
-	private ObjectProvider<DubboServiceMetadataRepository> dubboServiceMetadataRepository;
+	@Autowired(required = false)
+	private DubboServiceMetadataRepository dubboServiceMetadataRepository;
 
 	@Autowired
 	private JSONUtils jsonUtils;
 
 	@Override
 	public String getServiceRestMetadata() {
-		Set<ServiceRestMetadata> serviceRestMetadata = getRepository()
-				.getServiceRestMetadata();
+		if (this.dubboServiceMetadataRepository == null) {
+			return null;
+		}
+		Set<ServiceRestMetadata> serviceRestMetadata = this.dubboServiceMetadataRepository.getServiceRestMetadata();
 		String serviceRestMetadataJsonConfig = null;
 		if (!isEmpty(serviceRestMetadata)) {
 			serviceRestMetadataJsonConfig = jsonUtils.toJSON(serviceRestMetadata);
@@ -63,12 +64,18 @@ public class IntrospectiveDubboMetadataService implements DubboMetadataService {
 
 	@Override
 	public Set<String> getAllServiceKeys() {
-		return getRepository().getAllServiceKeys();
+		if (this.dubboServiceMetadataRepository == null) {
+			return Collections.emptySet();
+		}
+		return this.dubboServiceMetadataRepository.getAllServiceKeys();
 	}
 
 	@Override
 	public Map<String, String> getAllExportedURLs() {
-		Map<String, List<URL>> allExportedUrls = getRepository().getAllExportedUrls();
+		if (this.dubboServiceMetadataRepository == null) {
+			return Collections.emptyMap();
+		}
+		Map<String, List<URL>> allExportedUrls = this.dubboServiceMetadataRepository.getAllExportedUrls();
 		if (isEmpty(allExportedUrls)) {
 			if (logger.isDebugEnabled()) {
 				logger.debug("There is no registered URL.");
@@ -87,13 +94,11 @@ public class IntrospectiveDubboMetadataService implements DubboMetadataService {
 
 	@Override
 	public String getExportedURLs(String serviceInterface, String group, String version) {
-		List<URL> urls = getRepository().getExportedURLs(serviceInterface, group,
-				version);
+		if (this.dubboServiceMetadataRepository == null) {
+			return null;
+		}
+		List<URL> urls = this.dubboServiceMetadataRepository.getExportedURLs(serviceInterface, group, version);
 		return jsonUtils.toJSON(urls);
-	}
-
-	private DubboServiceMetadataRepository getRepository() {
-		return dubboServiceMetadataRepository.getIfAvailable();
 	}
 
 }
