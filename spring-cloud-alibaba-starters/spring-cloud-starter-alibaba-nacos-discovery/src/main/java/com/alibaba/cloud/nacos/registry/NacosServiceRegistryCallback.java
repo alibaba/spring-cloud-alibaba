3510e55e3105a8679dd39f2eda7d84936d30e540
weichangjin <469753862@qq.com>
2022-05-06 23:17:31 +0800

    reformat code

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-nacos-discovery/src/main/java/com/alibaba/cloud/nacos/registry/NacosServiceRegistryCallback.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-nacos-discovery/src/main/java/com/alibaba/cloud/nacos/registry/NacosServiceRegistryCallback.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *	  https://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,12 +23,12 @@ package com.alibaba.cloud.nacos.registry;
 public interface NacosServiceRegistryCallback {
 
 	/**
-	 * Nacos Service Registry finished Callback
+	 * Nacos Service Registry finished Callback.
 	 */
 	void success();
 
 	/**
-	 * Nacos Service Registry fail Callback
+	 * Nacos Service Registry fail Callback.
 	 */
 	void fail();
 }
