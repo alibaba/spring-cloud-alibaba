9512166536fdfb38a985a097f66f8a0f59b7f1cf
sorie <gsoriee@gmail.com>
2022-05-11 21:37:56 +0800

    remove timed message example from rocketmq examples. Because it is not supported by rocketmq community version now.

--- spring-cloud-alibaba-examples/rocketmq-example/rocketmq-delay-consume-example/src/main/java/com/alibaba/cloud/examples/delay/RocketMQDelayConsumeApplication.java
+++ spring-cloud-alibaba-examples/rocketmq-example/rocketmq-delay-consume-example/src/main/java/com/alibaba/cloud/examples/delay/RocketMQDelayConsumeApplication.java
@@ -58,25 +58,8 @@ public class RocketMQDelayConsumeApplication {
 				Map<String, Object> headers = new HashMap<>();
 				headers.put(MessageConst.PROPERTY_KEYS, key);
 				headers.put(MessageConst.PROPERTY_ORIGIN_MESSAGE_ID, i);
-				headers.put("DELAY", 2);
+				headers.put(MessageConst.PROPERTY_DELAY_TIME_LEVEL, 2);
 				Message<SimpleMsg> msg = new GenericMessage(new SimpleMsg("Delay RocketMQ " + i), headers);
-				streamBridge.send("producer-out-0", msg);
-			}
-		};
-	}
-
-	@Bean
-	public ApplicationRunner producerSchedule() {
-		return args -> {
-			for (int i = 0; i < 100; i++) {
-				String key = "KEY" + i;
-				Map<String, Object> headers = new HashMap<>();
-				headers.put(MessageConst.PROPERTY_KEYS, key);
-				headers.put(MessageConst.PROPERTY_ORIGIN_MESSAGE_ID, i);
-				// send message after 3000ms
-				long delayTime = System.currentTimeMillis() + 3000;
-				headers.put(MessageConst.PROPERTY_CONSUME_START_TIMESTAMP, delayTime);
-				Message<SimpleMsg> msg = new GenericMessage(new SimpleMsg("Schedule RocketMQ " + i), headers);
 				streamBridge.send("producer-out-0", msg);
 			}
 		};
