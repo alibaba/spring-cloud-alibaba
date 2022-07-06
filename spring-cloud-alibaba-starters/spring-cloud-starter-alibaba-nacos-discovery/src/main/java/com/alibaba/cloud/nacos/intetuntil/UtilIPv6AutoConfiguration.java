ee36dfe18f49e802e74d6eaf9d9e8fe7148cfb7e
lai hui <1353307710@qq.com>
2022-07-06 22:30:53 +0800

    fix some question

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-nacos-discovery/src/main/java/com/alibaba/cloud/nacos/intetuntil/UtilIPv6AutoConfiguration.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-nacos-discovery/src/main/java/com/alibaba/cloud/nacos/intetuntil/UtilIPv6AutoConfiguration.java
@@ -1,11 +1,16 @@
 package com.alibaba.cloud.nacos.intetuntil;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import com.alibaba.cloud.nacos.ConditionalOnNacosDiscoveryEnabled;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.client.ConditionalOnDiscoveryEnabled;
 import org.springframework.cloud.commons.util.InetUtilsProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnDiscoveryEnabled
+@ConditionalOnNacosDiscoveryEnabled
 public class UtilIPv6AutoConfiguration {
     public UtilIPv6AutoConfiguration() {
     }
