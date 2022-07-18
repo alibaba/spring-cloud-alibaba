afc8f078790da401c9f491a125cfee15268f627d
lai hui <1353307710@qq.com>
2022-07-18 14:54:01 +0800

    fix

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-nacos-discovery/src/main/java/com/alibaba/cloud/nacos/inetutil/InetIPv6Util.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-nacos-discovery/src/main/java/com/alibaba/cloud/nacos/inetutil/InetIPv6Util.java
@@ -16,11 +16,6 @@
 
 package com.alibaba.cloud.nacos.inetutil;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.cloud.commons.util.InetUtils;
-import org.springframework.cloud.commons.util.InetUtilsProperties;
-
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.Inet6Address;
@@ -33,6 +28,12 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.commons.util.InetUtilsProperties;
 
 /**
  * @author HH
