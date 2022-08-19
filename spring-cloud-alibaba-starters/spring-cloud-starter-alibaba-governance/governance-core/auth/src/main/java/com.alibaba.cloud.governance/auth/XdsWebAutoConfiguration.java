54b63d42ed8851daac4f364f7cfc608a464c6943
musi <liuziming.lzm@alibaba-inc.com>
2022-08-19 19:14:18 +0800

    feat: Interception point of spring

---
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-governance/governance-core/auth/src/main/java/com.alibaba.cloud.governance/auth/XdsWebAutoConfiguration.java
@@ -0,0 +1,18 @@
+package com.alibaba.cloud.governance.auth;
+
+import com.alibaba.cloud.governance.auth.webmvc.AuthWebInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+@ConditionalOnProperty(name = "spring.cloud.governance.auth.enabled", matchIfMissing = true)
+public class XdsWebAutoConfiguration {
+    @Bean
+    @ConditionalOnProperty(name = "spring.cloud.governance.auth.enabled")
+    public AuthWebInterceptor authWebInterceptor() {
+        return new AuthWebInterceptor();
+    }
+}
---
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-governance/governance-core/auth/src/main/java/com.alibaba.cloud.governance/auth/XdsWebFluxAutoConfiguration.java
@@ -0,0 +1,17 @@
+package com.alibaba.cloud.governance.auth;
+import com.alibaba.cloud.governance.auth.webflux.AuthWebFluxFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+@ConditionalOnProperty(name = "spring.cloud.governance.auth.enabled", matchIfMissing = true)
+public class XdsWebFluxAutoConfiguration {
+    @Bean
+    @ConditionalOnProperty(name = "spring.cloud.governance.auth.enabled")
+    public AuthWebFluxFilter authWebFluxFilter() {
+        return new AuthWebFluxFilter();
+    }
+}
