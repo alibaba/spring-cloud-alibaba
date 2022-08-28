bc5164108cf8e04a5a0a7ad27a18677a58ba89ff
Freeman Lau <llw599502537@gmail.com>
2022-08-29 01:40:36 +0800

    Sentinel flow control test

---
+++ spring-cloud-alibaba-tests/sentinel-tests/sentinel-flowcontrol-test/src/test/java/com/alibaba/cloud/tests/sentinel/flowcontrol/SentinelFlowControlTestAppTest.java
@@ -0,0 +1,49 @@
+package com.alibaba.cloud.tests.sentinel.flowcontrol;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class SentinelFlowControlTestAppTest {
+
+	@LocalServerPort
+	int port;
+
+	@Test
+	public void testFlowControl() throws InterruptedException {
+		RestTemplate rest = new RestTemplate();
+
+		final int threadCount = 3;
+		CountDownLatch latch = new CountDownLatch(threadCount);
+		List<String> result = new ArrayList<>();
+
+		for (int i = 0; i < threadCount; i++) {
+			new Thread(() -> {
+				try {
+					ResponseEntity<String> res = rest.getForEntity(
+							"http://localhost:" + port + "/flowControl", String.class);
+					result.add(res.getBody());
+				} finally {
+					latch.countDown();
+				}
+			}).start();
+		}
+
+		latch.await();
+
+		assertThat(result).isEmpty();
+//		assertThat(result).anyMatch(s -> s.contains("Blocked by Sentinel"));
+	}
+
+}
