85874e46773a627e95c0d92718d77d6794e8a5c9
windwheel <yuluoxinsheng@gmail.com>
2022-08-28 10:05:50 +0800

    fix checkstyle

--- spring-cloud-alibaba-tests/rocketmq-tests/rocketmq-stream-test/src/test/java/com/alibaba/cloud/stream/binder/rocketmq/RocketmqProduceAndConsumerTests.java
+++ spring-cloud-alibaba-tests/rocketmq-tests/rocketmq-stream-test/src/test/java/com/alibaba/cloud/stream/binder/rocketmq/RocketmqProduceAndConsumerTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.cloud.stream.binder.rocketmq;
 
 import java.util.HashMap;
@@ -50,7 +66,7 @@ import static org.springframework.cloud.stream.test.matcher.MessageQueueMatcher.
 		"spring.cloud.stream.bindings.uppercaseFunction-in-0.content-type=application/json",
 		"spring.cloud.stream.bindings.uppercaseFunction-in-0.group=test-group1",
 		"spring.cloud.stream.bindings.uppercaseFunction-in-0.consumer.push.orderly=true",
-		"spring.cloud.stream.bindings.uppercaseFunction-in-0.consumer.maxAttempts=1", })
+		"spring.cloud.stream.bindings.uppercaseFunction-in-0.consumer.maxAttempts=1" })
 public class RocketmqProduceAndConsumerTests {
 
 	@Autowired
