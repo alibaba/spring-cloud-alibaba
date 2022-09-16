0cbc7305b2f598db3c8e03d5a71ac4242c1a4d84
lai hui <1353307710@qq.com>
2022-09-16 21:27:26 +0800

    feat: add control surface interface

---
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-governance/governance-core/label-routing/label-routing-data/src/main/java/com/alibaba/cloud/data/controlsurface/ControlSurfaceConnectionAutoConfiguration.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.cloud.data.controlsurface;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author HH
+ */
+@EnableAutoConfiguration
+public class ControlSurfaceConnectionAutoConfiguration {
+
+	@Bean
+	@ConditionalOnMissingBean
+	public ControlSurfaceConnection controlSurfaceConnection() {
+		return new ControlSurfaceConnection();
+	}
+}
