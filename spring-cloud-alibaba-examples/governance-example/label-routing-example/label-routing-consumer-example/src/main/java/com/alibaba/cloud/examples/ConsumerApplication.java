591d37092311c4405fc9b785bac4d349150c4108
lai hui <1353307710@qq.com>
2022-09-24 09:32:37 +0800

    feat: add example

---
+++ spring-cloud-alibaba-examples/governance-example/label-routing-example/label-routing-consumer-example/src/main/java/com/alibaba/cloud/examples/ConsumerApplication.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.cloud.examples;
+
+
+import com.alibaba.cloud.router.ribbon.LabelRouteRule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author HH
+ */
+@SpringBootApplication
+@EnableDiscoveryClient(autoRegister = true)
+@EnableFeignClients
+@RibbonClient(name = "service-provider", configuration = LabelRouteRule.class)
+public class ConsumerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+	@FeignClient(name = "service-provider")
+	public interface FeignService {
+
+		/**
+		 * Feign test.
+		 * @return String
+		 */
+		@GetMapping("/test")
+		String test();
+
+	}
+
+	@RestController
+	public class Controller {
+
+		@Autowired
+		private ConsumerApplication.FeignService feignService;
+
+		@GetMapping("/router-test")
+		public String notFound() {
+			return feignService.test();
+		}
+	}
+}
