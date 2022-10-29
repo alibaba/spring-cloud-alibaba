082b79398fbc56e7c689507bb4df0866a4feb8e4
黄sir <2845964844@qq.com>
2022-10-29 21:16:31 +0800

    keep config and register group all the same as 'integrated-example'

--- spring-cloud-alibaba-examples/integrated-example/scripts/nacos-config-quick.sh
+++ spring-cloud-alibaba-examples/integrated-example/scripts/nacos-config-quick.sh
@@ -8,7 +8,7 @@ gatewayConfig=$(cat ../config/integrated-gateway.yaml)
 providerConfig=$(cat ../config/integrated-provider.yaml)
 consumerConfig=$(cat ../config/integrated-consumer.yaml)
 groupId="integrated-example"
-curl -X POST "nacos-server:8848/nacos/v1/cs/configs" -d "dataId=datasource-config.yaml&group=DEFAULT_GROUP&content=${datasourceConfig}"
+curl -X POST "nacos-server:8848/nacos/v1/cs/configs" -d "dataId=datasource-config.yaml&group=${groupId}&content=${datasourceConfig}"
 curl -X POST "nacos-server:8848/nacos/v1/cs/configs" -d "dataId=integrated-storage.yaml&group=${groupId}&content=${storageConfig}"
 curl -X POST "nacos-server:8848/nacos/v1/cs/configs" -d "dataId=integrated-account.yaml&group=${groupId}&content=${accountConfig}"
 curl -X POST "nacos-server:8848/nacos/v1/cs/configs" -d "dataId=integrated-order.yaml&group=${groupId}&content=${orderConfig}"
