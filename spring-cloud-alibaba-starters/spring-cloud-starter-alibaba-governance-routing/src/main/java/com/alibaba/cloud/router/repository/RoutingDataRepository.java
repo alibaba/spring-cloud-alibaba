4df47c405fcff1fc651df508c7d47192eaa7cd01
hui lai <1353307710@qq.com>
2023-01-10 17:29:32 +0800

    update import order

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-governance-routing/src/main/java/com/alibaba/cloud/router/repository/RoutingDataRepository.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-governance-routing/src/main/java/com/alibaba/cloud/router/repository/RoutingDataRepository.java
@@ -21,8 +21,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-import com.alibaba.cloud.commons.governance.routing.RoutingRule;
 import com.alibaba.cloud.commons.governance.routing.MatchService;
+import com.alibaba.cloud.commons.governance.routing.RoutingRule;
 import com.alibaba.cloud.commons.governance.routing.UnifiedRoutingDataStructure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
