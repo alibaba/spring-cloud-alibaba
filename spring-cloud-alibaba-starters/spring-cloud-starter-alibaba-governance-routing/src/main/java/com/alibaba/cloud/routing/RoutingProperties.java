7ca2f15ae3a5059e5db2cb1d79832ce3f2a441eb
Liu Ziming <liuziming.lzm@alibaba-inc.com>
2023-01-12 00:56:19 +0800

    2.2.x fix npe (#3085)
    * fix: fix npe in routing and reverse the flag in observeListeners
    
    * refactor: route -> routing

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-governance-routing/src/main/java/com/alibaba/cloud/routing/RoutingProperties.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-governance-routing/src/main/java/com/alibaba/cloud/routing/RoutingProperties.java
@@ -16,6 +16,11 @@
 
 package com.alibaba.cloud.routing;
 
+import javax.annotation.PostConstruct;
+
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.alibaba.cloud.routing.util.LoadBalanceUtil;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -28,12 +33,19 @@ public class RoutingProperties {
 	/**
 	 * Properties prefix.
 	 */
-	public static final String PROPERTY_PREFIX = "spring.cloud.governance.router";
+	public static final String PROPERTY_PREFIX = "spring.cloud.governance.routing";
 
 	/**
 	 * Load Balance Rule.
 	 */
 	private String rule;
+
+	@PostConstruct
+	public void init() {
+		if (StringUtils.isEmpty(rule)) {
+			rule = LoadBalanceUtil.ZONE_AVOIDANCE_RULE;
+		}
+	}
 
 	public String getRule() {
 		return rule;
