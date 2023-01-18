14d728083ea5fb1b2c6b355f6cf0a43ca0b0fb0d
YuLuo <1481556636@qq.com>
2023-01-18 17:56:06 +0800

    fix checkstyle error

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-appactive/src/main/java/com/alibaba/cloud/appactive/consumer/WebClientInterceptor.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-appactive/src/main/java/com/alibaba/cloud/appactive/consumer/WebClientInterceptor.java
@@ -26,8 +26,8 @@ import org.slf4j.Logger;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author yuluo
