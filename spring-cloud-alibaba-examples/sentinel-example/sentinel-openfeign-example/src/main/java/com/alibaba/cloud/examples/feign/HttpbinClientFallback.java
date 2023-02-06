bd4d3e21b25db232696ce857d66b5498ba2ba95f
chengpu <raozihao.rzh@alibaba-inc.com>
2023-02-06 17:31:12 +0800

    refactor: modified Sentinel OpenFeign & RestTemplate examples

---
+++ spring-cloud-alibaba-examples/sentinel-example/sentinel-openfeign-example/src/main/java/com/alibaba/cloud/examples/feign/HttpbinClientFallback.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2013-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.cloud.examples.feign;
+
+
+/**
+ * When the service is blown, the fallback operation is performed.
+ *
+ * @author raozihao
+ */
+public class HttpbinClientFallback implements HttpbinClient {
+
+	@Override
+	public String delay() {
+		return "delay fallback";
+	}
+
+	@Override
+	public String status404() {
+		return "404 fallback";
+	}
+
+	@Override
+	public String status500() {
+		return "500 fallback";
+	}
+
+	@Override
+	public String get() {
+		return "get fallback";
+	}
+
+	private Throwable throwable;
+
+	HttpbinClientFallback(Throwable throwable) {
+		this.throwable = throwable;
+	}
+}
