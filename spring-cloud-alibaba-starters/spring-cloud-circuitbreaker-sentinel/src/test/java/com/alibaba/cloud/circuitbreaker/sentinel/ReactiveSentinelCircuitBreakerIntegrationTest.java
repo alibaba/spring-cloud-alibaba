6c87d18b6d74df65d200b62b44b45079de6a65f9
ruansheng <chrisruans@gmail.com>
2023-03-20 23:43:24 +0800

    commit

--- spring-cloud-alibaba-starters/spring-cloud-circuitbreaker-sentinel/src/test/java/com/alibaba/cloud/circuitbreaker/sentinel/ReactiveSentinelCircuitBreakerIntegrationTest.java
+++ spring-cloud-alibaba-starters/spring-cloud-circuitbreaker-sentinel/src/test/java/com/alibaba/cloud/circuitbreaker/sentinel/ReactiveSentinelCircuitBreakerIntegrationTest.java
@@ -79,6 +79,7 @@ public class ReactiveSentinelCircuitBreakerIntegrationTest {
 		StepVerifier.create(service.slowFlux(idx)).expectNext("slowflux").verifyComplete();
 		StepVerifier.create(service.slowFlux(idx)).expectNext("slowflux").verifyComplete();
 		StepVerifier.create(service.slowFlux(idx)).expectNext("slowflux").verifyComplete();
+		Thread.sleep(300);
 		StepVerifier.create(service.slowFlux(idx)).expectNext("flux_fallback")
 				.verifyComplete();
 	}
