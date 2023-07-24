02fff4c6896bfded53fef7280c66a4d51d7622dd
chengpu <raozihao.rzh@alibaba-inc.com>
2023-07-24 23:10:41 +0800

    Added neccesary annotation in example

--- spring-cloud-alibaba-examples/nacos-example/nacos-gateway-example/nacos-gateway-discovery-example/src/main/java/com/alibaba/cloud/examples/GatewayApplication.java
+++ spring-cloud-alibaba-examples/nacos-example/nacos-gateway-example/nacos-gateway-discovery-example/src/main/java/com/alibaba/cloud/examples/GatewayApplication.java
@@ -19,12 +19,17 @@ package com.alibaba.cloud.examples;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 
 /**
  * @author lengleng
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@LoadBalancerClients({
+		@LoadBalancerClient("service-gateway-provider")
+})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
