d176c6716ebd820bd55b9af261446cf9f4b1b76d
moremind <hefengen@hotmail.com>
2023-12-22 20:25:38 +0800

    [type:fix] fix nacosServiceInstance scheme npe (#3545)
    * fix nacos scheme npe

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-nacos-discovery/src/main/java/com/alibaba/cloud/nacos/NacosServiceInstance.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-nacos-discovery/src/main/java/com/alibaba/cloud/nacos/NacosServiceInstance.java
@@ -68,6 +68,11 @@ public class NacosServiceInstance implements ServiceInstance {
 		return metadata;
 	}
 
+	@Override
+	public String getScheme() {
+		return getUri().getScheme();
+	}
+
 	public void setServiceId(String serviceId) {
 		this.serviceId = serviceId;
 	}
