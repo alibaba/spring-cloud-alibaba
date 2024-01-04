38679e19478b5b07b72759e7c0e02e7c86306520
YuLuo <1481556636@qq.com>
2024-01-04 19:56:28 +0800

    feat: add license header for sql scripts files (#3564)

--- spring-cloud-alibaba-examples/integrated-example/config-init/sql/init.sql
+++ spring-cloud-alibaba-examples/integrated-example/config-init/sql/init.sql
@@ -1,3 +1,35 @@
+-- 
+-- Copyright 2013-2023 the original author or authors.
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      https://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- 
+
+--
+-- Copyright 2013-2023 the original author or authors.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      https://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 -- Storage库存微服务的数据库业务初始化
 DROP DATABASE IF EXISTS integrated_storage;
 CREATE DATABASE integrated_storage;
