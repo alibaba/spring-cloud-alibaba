a3c6bffeaac83b59e606f0cca267c6e42be8f7eb
ruansheng <chrisruans@gmail.com>
2024-01-22 14:47:19 +0800

    add capacity limit for nacos config snapshots

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-nacos-config/src/main/java/com/alibaba/cloud/nacos/refresh/NacosSnapshotConfigManager.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-nacos-config/src/main/java/com/alibaba/cloud/nacos/refresh/NacosSnapshotConfigManager.java
@@ -1,7 +1,11 @@
 package com.alibaba.cloud.nacos.refresh;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author: ruansheng
@@ -9,8 +13,10 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class NacosSnapshotConfigManager {
 
-	private static final Map<String, String> CONFIG_INFO_SNAPSHOT_MAP = new ConcurrentHashMap<>(8);
+	private static final Logger log = LoggerFactory.getLogger(NacosSnapshotConfigManager.class);
 
+	private static final Map<String, String> CONFIG_INFO_SNAPSHOT_MAP = new ConcurrentHashMap<>(8);
+	private static final int MAX_SNAPSHOT_COUNT = 100;
 
 	private static String formatConfigSnapshotKey(String dataId, String group) {
 		return dataId + "@" + group;
@@ -21,7 +27,19 @@ public class NacosSnapshotConfigManager {
 	}
 
 	public static void putConfigSnapshot(String dataId, String group, String configInfo) {
-		CONFIG_INFO_SNAPSHOT_MAP.put(formatConfigSnapshotKey(dataId, group), configInfo);
+		try {
+			// Theoretically, the capacity limit restriction will never be triggered.
+			// This portion of the code logic serves as an additional fault tolerance layer.
+			if (CONFIG_INFO_SNAPSHOT_MAP.size() > MAX_SNAPSHOT_COUNT) {
+				Iterator<Map.Entry<String, String>> iterator = CONFIG_INFO_SNAPSHOT_MAP.entrySet().iterator();
+				iterator.next();
+				iterator.remove();
+			}
+			CONFIG_INFO_SNAPSHOT_MAP.put(formatConfigSnapshotKey(dataId, group), configInfo);
+		}
+		catch (Exception e) {
+			log.warn("remove nacos config snapshot error", e);
+		}
 	}
 
 	public static void removeConfigSnapshot(String dataId, String group) {
