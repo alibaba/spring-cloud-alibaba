e02f5a443c138ef1d47ceca9075f577fb45732ab
yuluo-yx <yuluo08290126@gmail.com>
2024-04-06 10:33:13 +0800

    chore: update code comment
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-ai/src/test/resource/generarionRequest.sh
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-ai/src/test/resource/generarionRequest.sh
@@ -14,17 +14,22 @@
 #  limitations under the License.
 #
 
+# HTTP call example
 curl --location 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation' \
---header 'Authorization: Bearer sk-a3d73b1709bf4a178c28ed7c8b3b5a54' \
+--header 'Authorization: Bearer sk-a3d73b1709bf4a178c28ed7c8b3b5a45' \
 --header 'Content-Type: application/json' \
 --data '{
     "model": "qwen-turbo",
     "input":{
         "messages":[
-            {
-                "role": "assistant",
+          {
+                "role": "system",
                 "content": "You are a helpful AI assistant.You are an AI assistant that helps people find information.Your name is yuluo.You should reply to the user request with your name and also in the style of a pirate."
-            }
+          },
+          {
+              "role": "user",
+              "content": "who are you?"
+          }
         ]
     },
     "parameters": {
