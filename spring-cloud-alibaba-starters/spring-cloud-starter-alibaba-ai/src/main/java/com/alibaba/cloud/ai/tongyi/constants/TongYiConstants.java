fd621dfdbba346c4a7c950d531bbd612aa21fdd6
yuluo-yx <yuluo08290126@gmail.com>
2024-05-01 21:09:31 +0800

    fix: add java docs
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-ai/src/main/java/com/alibaba/cloud/ai/tongyi/constants/TongYiConstants.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-ai/src/main/java/com/alibaba/cloud/ai/tongyi/constants/TongYiConstants.java
@@ -26,8 +26,14 @@ public final class TongYiConstants {
 	private TongYiConstants() {
 	}
 
+	/**
+	 * Spring Cloud Alibaba AI constants prefix.
+	 */
 	public static final String SCA_AI = "SPRING_CLOUD_ALIBABA_";
 
+	/**
+	 * TongYi AI apikey env name.
+	 */
 	public static final String SCA_AI_TONGYI_API_KEY = SCA_AI + "TONGYI_API_KEY";
 
 }
