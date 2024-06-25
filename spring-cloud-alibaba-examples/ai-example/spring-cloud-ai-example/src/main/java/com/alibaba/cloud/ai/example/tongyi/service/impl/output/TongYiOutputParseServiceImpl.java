0b69c293eb513fa6bd5d0d60262218fdd9acd499
zhangqian9158 <zhangqian9158@gmail.com>
2024-06-25 09:19:23 +0800

    upgrade spring ai version to 1.0.0-M1

--- spring-cloud-alibaba-examples/ai-example/spring-cloud-ai-example/src/main/java/com/alibaba/cloud/ai/example/tongyi/service/impl/output/TongYiOutputParseServiceImpl.java
+++ spring-cloud-alibaba-examples/ai-example/spring-cloud-ai-example/src/main/java/com/alibaba/cloud/ai/example/tongyi/service/impl/output/TongYiOutputParseServiceImpl.java
@@ -29,7 +29,7 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.parser.BeanOutputParser;
+import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.stereotype.Service;
 
 /**
@@ -56,7 +56,7 @@ public class TongYiOutputParseServiceImpl extends AbstractTongYiServiceImpl {
 	@Override
 	public ActorsFilms genOutputParse(String actor) {
 
-		var outputParser = new BeanOutputParser<>(ActorsFilms.class);
+		var outputParser = new BeanOutputConverter<>(ActorsFilms.class);
 
 		String format = outputParser.getFormat();
 		logger.info("format: " + format);
