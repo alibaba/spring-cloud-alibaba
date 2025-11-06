447897e2914402e544af47630d6910d8180fc803
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2025-11-06 17:55:36 +0800

    :bug: fix WebClientCustomizer package

--- spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-seata/src/main/java/com/alibaba/cloud/seata/webclient/SeataWebClientBuilderCustomizer.java
+++ spring-cloud-alibaba-starters/spring-cloud-starter-alibaba-seata/src/main/java/com/alibaba/cloud/seata/webclient/SeataWebClientBuilderCustomizer.java
@@ -17,7 +17,7 @@
 package com.alibaba.cloud.seata.webclient;
 
 
-import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.cloud.client.loadbalancer.reactive.WebClientCustomizer;
 import org.springframework.web.reactive.function.client.WebClient;
 
 /**
