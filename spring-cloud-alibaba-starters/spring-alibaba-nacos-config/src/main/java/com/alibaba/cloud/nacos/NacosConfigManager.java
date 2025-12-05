4eca45f603df79701f2f2dec95dd04f9780f6825
xzxiaoshan <365384722@qq.com>
2025-12-05 16:44:33 +0800

    fix: NacosConfigDataLocationResolver 在未使用 optional:nacos: 进行 import 时，Spring 中 NacosConfigManager Bean 实例属性为默认值问题  (issue #4147)

--- spring-cloud-alibaba-starters/spring-alibaba-nacos-config/src/main/java/com/alibaba/cloud/nacos/NacosConfigManager.java
+++ spring-cloud-alibaba-starters/spring-alibaba-nacos-config/src/main/java/com/alibaba/cloud/nacos/NacosConfigManager.java
@@ -18,6 +18,8 @@ package com.alibaba.cloud.nacos;
 
 import java.util.Objects;
 
+import com.alibaba.cloud.nacos.configdata.NacosConfigDataLoadProperties;
+import com.alibaba.cloud.nacos.configdata.NacosConfigDataLocationResolver;
 import com.alibaba.cloud.nacos.diagnostics.analyzer.NacosConnectionFailureException;
 import com.alibaba.nacos.api.NacosFactory;
 import com.alibaba.nacos.api.config.ConfigService;
@@ -25,18 +27,36 @@ import com.alibaba.nacos.api.exception.NacosException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.context.properties.bind.BindHandler;
+import org.springframework.boot.context.properties.bind.Bindable;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.lang.NonNull;
+
 /**
  * @author zkzlx
  */
-public class NacosConfigManager {
+public class NacosConfigManager implements InitializingBean, EnvironmentAware {
 
 	private static final Logger log = LoggerFactory.getLogger(NacosConfigManager.class);
 
 	private static ConfigService service;
 
-	private static NacosConfigManager INSTANCE;
+	private static volatile NacosConfigManager INSTANCE;
 
 	private NacosConfigProperties nacosConfigProperties;
+
+	/**
+	 * The Spring environment used to bind properties.
+	 */
+	private Environment environment;
+
+	/*
+	 * Handler for binding configuration properties.
+	 */
+	private static BindHandler bindHandler;
 
 	public NacosConfigManager(NacosConfigProperties nacosConfigProperties) {
 		this.nacosConfigProperties = nacosConfigProperties;
@@ -87,6 +107,81 @@ public class NacosConfigManager {
 
 	public NacosConfigProperties getNacosConfigProperties() {
 		return nacosConfigProperties;
+	}
+
+	/**
+	 * Sets the Spring environment, used for property binding.
+	 *
+	 * @param environment the Spring environment
+	 */
+	@Override
+	public void setEnvironment(@NonNull Environment environment) {
+		this.environment = environment;
+	}
+
+	/**
+	 * Called after properties are set, re-binds configuration from the environment
+	 * and updates the internal configuration properties and ConfigService accordingly.
+	 */
+	@Override
+	public void afterPropertiesSet() {
+		// Rebinds environment properties to NacosConfigProperties
+		Binder binder = Binder.get(environment);
+		String nacosPrefix = NacosPropertiesPrefixer.getPrefix(binder);
+		String nacosConfigPrefix = nacosPrefix + ".config";
+		NacosConfigProperties newProperties = binder
+				.bind(nacosPrefix, Bindable.of(NacosConfigDataLoadProperties.class), bindHandler)
+				.map(properties -> binder
+						.bind(nacosConfigPrefix, Bindable.ofInstance(properties), bindHandler)
+						.orElse(properties))
+				.orElseGet(() -> binder
+						.bind(nacosConfigPrefix, Bindable.of(NacosConfigDataLoadProperties.class), bindHandler)
+						.orElse(null));
+		if (Objects.nonNull(newProperties)) {
+			// Update current instance's properties
+			this.nacosConfigProperties = newProperties;
+			// Safely update singleton instance
+			if (INSTANCE != null) {
+				synchronized (NacosConfigManager.class) {
+					if (INSTANCE != null) {
+						INSTANCE.nacosConfigProperties = newProperties;
+						// Recreate ConfigService if needed
+						recreateConfigService(newProperties);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Recreates the ConfigService using new properties after shutting down the previous one.
+	 *
+	 * @param properties the new Nacos config properties
+	 */
+	private void recreateConfigService(NacosConfigProperties properties) {
+		if (service != null) {
+			service = null;
+		}
+		createConfigService(properties);
+	}
+
+	/**
+	 * Sets the bind handler for configuration properties binding.
+	 * <p>This method is typically called during early Spring startup phase,
+	 * such as in {@link NacosConfigDataLocationResolver} class.</p>
+	 *
+	 * @param handler the bind handler to set
+	 */
+	public static void setBindHandler(BindHandler handler) {
+		bindHandler = handler;
+	}
+
+	/**
+	 * Get BindHandler.
+	 *
+	 */
+	public static BindHandler getBindHandler() {
+		return bindHandler;
 	}
 
 }
